e625b421e2fc3f23d5e6970e09b5c15dfc11d59e
Pontorez <pontorez@pontorez.com>
2018-12-27 09:37:59 -0500

    New version of SwiftMailer API

--- sendMessages.php
+++ sendMessages.php
@@ -1,50 +1,36 @@
 #!/usr/bin/env php
 <?php
 
-require_once 'vendor/swiftmailer/swiftmailer/lib/swift_required.php';
+require_once './vendor/autoload.php';
 
-$transport = Swift_SmtpTransport::newInstance('localhost', 25);
+$transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
+    ->setUsername('username@gmail.com')
+    ->setPassword('password');
+
 // Create the Mailer using your created Transport
-$mailer = Swift_Mailer::newInstance($transport);
+$mailer = new Swift_Mailer($transport);
 
 $targetAddresses = file_get_contents('emails.txt');
 $targetAddresses = explode("\n", trim($targetAddresses));
 
 foreach ($targetAddresses as $i => $emailTo) {
-    $message = Swift_Message::newInstance()
+    $message = new Swift_Message();
+    $message
         // Give the message a subject
-        ->setSubject('Последние новости')
+        ->setSubject('Merry Christmas')
         // Set the From address with an associative array
         ->setFrom(['sales@example.com' => 'Fantomas'])
-        ->setCc(['it@example.com', 'sales@example.com'])
-        ->setReturnPath('karl@example.com')
+        // ->setCc(['it@example.com', 'sales@example.com'])
+        ->setReturnPath('info@example.com')
         // Set the To addresses with an associative array
         //    ->setTo(['receiver@domain.org', 'other@domain.org' => 'A name'])
         ->setTo($emailTo);
         // Give it a body
 
-    $message->setBody(
-    '<html>' .
-    ' <head></head>' .
-    ' <body>' .
-    '<p>Здравствуйте!</p>
-    <p>Просим ознакомиться с сообщением в приложенных файлах.</p>
-    <p>С уважением,<br/>
-Карл Шустерлинг.</p>'.
-    '<p><img src="' .
-    $message->embed(Swift_Image::fromPath('attachments/index.jpg')) .
-    '" alt="Image" /><br/>' .
-    'Тел.: +7 (555) 555 55 55<br/>
-    E-mail: <a href="mailto:sales@example.com">sales@example.com</a></p>' .
-    '<small style="color:#404040">ООО «Государственный публичный дом имени Н.К. Крупской», ул. Отстойная, д. 13, стр. 42, Волчехуйск, Лугандония<br/>
-<a href="https://example.com/">www.example.com</a></small>'.
-    ' </body>' .
-    '</html>',
-    'text/html' // Mark the content-type as HTML
-);
-    // Optionally add any attachments
-    $message->attach(Swift_Attachment::fromPath('attachments/ru.pdf'));
-    $message->attach(Swift_Attachment::fromPath('attachments/en.pdf'));
+    $body = file_get_contents('tpl/xmas.html');
+    $message->setEncoder( new Swift_Mime_ContentEncoder_PlainContentEncoder('8bit'));
+
+    $message->setBody($body, 'text/html');
 
     echo "Sending to $emailTo\n";
     $result = $mailer->send($message);
